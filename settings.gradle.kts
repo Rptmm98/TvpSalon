b66d8520891af1d458fe8d73211c9a376c1434e5
Rptmm98 <bagp1234@gmail.com>
2026-04-28 00:37:51 -0400

    Add settings.gradle.kts file for TvpSalon project

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TvpSalon"
+include "app"
